a95a30e7a8b8b5f8d984ee47036351a1be1a1183
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-04-03 11:44:48 -0700

    Add files via upload

--- Dinosaur/app.js
+++ Dinosaur/app.js
@@ -9,6 +9,7 @@
       this.when = when;
       this.fact = fact   
     }
+    // Create Dino Compare Method 1
     Dino.prototype.compareHeight = function(human){
       var result = ''
       if(this.height > human.height){
@@ -19,9 +20,21 @@
         result += this.species + "has same height with you ";
       }
     }
+    // Create Dino Compare Method 2
     Dino.prototype.compareWeight = function(human){
       var result = ''
       if(this.weight > human.weight){
+        result += this.species + "is heigher than you";
+      }else if(this.weight > human.weight){
+        result += this.species + "is lower than you";
+      }else{
+        result += this.species + "has same weight with you ";
+      }
+    }
+    // Create Dino Compare Method 3
+    Dino.prototype.compareDiet = function(human){
+      var result = ''
+      if(this.diet > human.diet){
         result += this.species + "is heigher than you";
       }else if(this.weight > human.weight){
         result += this.species + "is lower than you";
@@ -36,17 +49,17 @@
       this.weight = weight
       this.diet = diet
      }  
+
     const getData = async () => {
       const fetchedData = await fetch("./dino.json");
       const data = await fetchedData.json();
+      for(i=0 ;i<8 ;i++){
+        const d = data.Dinos[i];
+        data.Dinos[i] = new Dino(d.species,d.weight,d.height,d.diet,d.where,d.when,d.fact);
+      }
       return data.Dinos;
     };
-    getData().then(data => {
-      console.log(data); // will print the array
-    }).catch(error => {
-      console.error(error);
-    });
-    // Create Human Object
+
     
     // Use IIFE to get human data from form
     document.getElementById("btn").addEventListener("click", function() {
@@ -61,21 +74,49 @@
         return human;
     })()
     console.log(human);
-    let compare1 = compareHeight(human,dino);
-    console.log(compare1);
-    });
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-    function compareHeight(human,dino){
-      human.height
-    }
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+    const data = getData().then(data => {
+      data.splice(4, 0, human);
+      console.log(data)
+      document.getElementById('dino-compare').remove();
+      const grid = document.getElementById('grid');
+      const table = document.createElement("table");
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+      for(i=0;i<3; i++){
+        const row = document.createElement("tr");
+        for(j=0;j<3; j++){
+          const cell = document.createElement("td");
+          console.log(data[i*3+j])
+          if(i*3+j != 4){
+            cell.innerHTML = `
+              <div class="grid-item">
+              <h3>${data[i*3+j].species}</h3>
+              <img src="images/${data[i*3+j].species.toLowerCase()}.png" alt="${data[i*3+j].species} image" />
+              <p>${data[i*3+j].fact ? data[i*3+j].fact : ""} 
+              ${data[i*3+j].weight ? data[i*3+j].compareWeight(human) : ""} 
+              ${data[i*3+j].height ? data[i*3+j].compareHeight(human) : ""}
+              </p>
+              `
+            }else{
+              cell.innerHTML = `
+              <div class="grid-item">
+              <h3>human</h3>
+              <img src="images/human.png" alt="human image" />
+              <p>${data[i*3+j].fact ? data[i*3+j].fact : ""} 
+              ${data[i*3+j].weight ? data[i*3+j].weight : ""} 
+              ${data[i*3+j].height ? data[i*3+j].height: ""}
+              </p>
+              `
+            }
+          row.append(cell);
+          }
+        table.append(row);
+      }
+      grid.append(table);
+    }).catch(error => {
+      console.error(error);
+    });
+    });
+
 
 
     // Generate Tiles for each Dino in Array
