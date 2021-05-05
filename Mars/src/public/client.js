ec7b67ae593da9722ea092e24dcc201371f6d5ef
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-05-05 16:56:57 -0700

    Update client.js

--- Mars/src/public/client.js
+++ Mars/src/public/client.js
@@ -1,4 +1,4 @@
-const store =  Immutable.Map({
+  const store =  Immutable.Map({
     user: Immutable.Map({
       name: "Human",
     }),
@@ -10,7 +10,9 @@ const store =  Immutable.Map({
 const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
+    
     store = store.merge(store, newState)
+    console.log(store.toJS())
     render(root, store)
 }
 
@@ -21,12 +23,13 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
+    state = state.toJS()
     let { rovers, apod } = state
-
+    
     return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
+
             <section>
                 <h3>Put things on the page!</h3>
                 <p>Here is an example section.</p>
@@ -72,33 +75,7 @@ const Greeting = (name) => {
     `
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
 
 
 const RoverInfo = (rover)=>{
@@ -143,6 +120,7 @@ const getImageOfTheDay = (state) => {
 }
 
 const getRoverData = (name) =>{
+    
     if(name){
     fetch(`http://localhost:3000/rovername/${name}`)
         .then(res => res.json())
